5eac3835227491d2f0702e4ed8ffac4117e214e3
rod-gxz <139155314+rod-gxz@users.noreply.github.com>
2025-09-04 15:41:14 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,7 @@
+
 questions =  [
-    {"id": 0, "question": "O Rodrigo e o Samuel deveriam transar.", "effects": {"auth": -10}}, 
+    {"id": 0, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"right": 10}},
+    {"id": 1, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"auth": 10}},
+    {"id": 2, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"prog": -10}},
+];
   
