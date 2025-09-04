0674a9468715cc486240a9f5d1cc00975151f1ac
rod-gxz <139155314+rod-gxz@users.noreply.github.com>
2025-09-04 15:45:49 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,7 +1,5 @@
-
-questions =  [
-    {"id": 0, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"right": 10}},
-    {"id": 1, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"auth": 10}},
-    {"id": 2, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"prog": -10}},
+questions = [
+    {"id": 0, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"right": 10, "auth": 0, "prog": 0}},
+    {"id": 1, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"right": 0, "auth": 10, "prog": 0}},
+    {"id": 2, "question": "O Rodrigo e o Samuel deviam transar.", "effects": {"right": 0, "auth": 0, "prog": -10}}
 ];
-  
