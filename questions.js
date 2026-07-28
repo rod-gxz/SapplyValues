1e746e082fa2426f048efb7e0a7fb593a768a1cc
Rodrigo Passos <139155314+rod-gxz@users.noreply.github.com>
2026-07-28 16:16:25 -0300

    Atualizar o questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,3 @@
 questions =  [
-    {"id": 0, "question": "Eu amo o Lenine, o Comunismo e o Elias Jabbour.", "effects": {"right": 10} {"prog": -10} {"auth": 10}},
+    {"id": 0, "question": "Eu amo o Lenine, o Comunismo e o Elias Jabbour.", "effects": {"right": 10} {"prog": -10} {"auth": 10}}
    ];
